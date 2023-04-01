c738ce52f6586f7f9fadc7492571edfedd834679
Jayabrata18 <jayabrata180402@gmail.com>
2023-04-01 18:16:35 +0530

    done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text,StatusBar, StyleSheet } from 'react-native'
 import React from 'react'
 import MusicPlayer from './screens/MusicPlayer'
 
 const App = () => {
   return (
     <View style={styles.container} >
+      <StatusBar barStyle="light-content"/>
       <MusicPlayer/>
     </View>
   )
